63dbe83d15a12df41218df3bc4a0f1d55ff87ed3
AlexWambui <alexwambuik@gmail.com>
2026-05-16 01:33:07 +0300

    added new svg and png favicons for the app shortcut.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="icon" href="{{ asset('assets/images/v2-white.svg') }}" sizes="any">
-        {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="{{ asset('assets/images/v2-white.svg') }}">
+
+        <link rel="icon" href="{{ asset('assets/images/v2-no-bg.png') }}" sizes="any">
+        <link rel="apple-touch-icon" href="{{ asset('assets/images/v2-no-bg.png') }}">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
